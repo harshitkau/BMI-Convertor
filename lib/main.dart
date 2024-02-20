2f085c2f8cf7d5d511e066f4da69303f0d52be72
harshitkau <88334621+harshitkau@users.noreply.github.com>
2024-02-21 00:20:18 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:ibmii/pages/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'IBMI',
+      routes: {
+        '/': (BuildContext _context) => MainPage(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
